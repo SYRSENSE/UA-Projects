ceb99f9a746b9318dd3e0fe16be26478e57778b9
SYRSENSE <133545215+SYRSENSE@users.noreply.github.com>
2024-05-09 23:46:49 -0400

    Add files via upload

--- Metodos/src/metodos/Menu.java
+++ Metodos/src/metodos/Menu.java
@@ -2,44 +2,60 @@ package metodos;
 import java.util.Scanner;
 public class Menu {
     Ordenamientos m = new Ordenamientos();
-    public void Menu(int [] array){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("[1] Burbuja");
-        System.out.println("[2] Insercion");
-        System.out.println("[3] Seleccion");
-        System.out.println("[4] ShellSort");
-        System.out.println("[5] MergeSort");
-        System.out.println("[6] QuickSort");
-        int op = sc.nextInt();
-        Opciones(op,array);
-    }
+    Scanner sc = new Scanner(System.in);
+    public void Menu(){
+        while (true){
+            int array[] = {54,26,93,17,77,31,44,55,20}; 
+            System.out.println("[1] Burbuja");
+            System.out.println("[2] Insercion");
+            System.out.println("[3] Seleccion");
+            System.out.println("[4] ShellSort");
+            System.out.println("[5] MergeSort");
+            System.out.println("[6] QuickSort");
+            System.out.println("[7] Salir");
+            int op = sc.nextInt();
+            if (op == 7) {
+                System.out.println("Saliendo del programa.");
+                break;
+            }
+            Opciones(op,array);
+        }
+ }
     public void Opciones(int op, int[] array){
         switch (op){
-            case 1:
-                System.out.println("Array Original: ");
+            case 1: 
                 m.BubleSort(array);
+                System.out.println("Complejidad O(n^2)");
                 break;
             case 2:
-                System.out.println("Array Original: ");
                 m.InsertionSort(array);
+                System.out.println("Complejidad O(n^2)");
                 break;
             case 3:
-                System.out.println("Array Original: ");
                 m.SelectionSort(array);
+                System.out.println("Complejidad O(n^2)");
                 break;
             case 4:
-                System.out.println("Array Original: ");
                 m.ShellSort(array);
+                System.out.println("Complejidad O(n(log(n))^2");
                 break;
             case 5:
                 System.out.println("Array Original: ");
+                m.MuestraArray(array);
                 m.mergeSort(array);
+                System.out.println("Complejidad O(n(log(n))");
                 break;
             case 6:
+                System.out.println("Array Original: ");
+                m.MuestraArray(array);
+                m.quicksort(array,0,array.length-1);
+                System.out.println("\nArray ordenado:");
+                m.MuestraArray(array);
+                System.out.println("Total comparaciones: " + m.get_comparisons_2());
+                System.out.println("Complejidad O(n(log(n))");
                 break;
             default:
-                break;
-                    
+                break;   
         }
     }
 }
--- Metodos/src/metodos/Ordenamientos.java
+++ Metodos/src/metodos/Ordenamientos.java
@@ -3,51 +3,69 @@ package metodos;
 
 
 public class Ordenamientos {
-    private void MuestraArray(int [] a){
+    public void MuestraArray(int [] a){
         for (int i=0;i<a.length;i++){
             System.out.print("["+a[i]+"]");
             }
         System.out.println("");
         System.out.println("");        
         }
-    public void BubleSort(int [] a){
+    public void BubleSort(int[] a) {
+        System.out.println("Array Original: ");
         this.MuestraArray(a);
         int n = a.length;  
-        int temp = 0;
+        int temp;
         int comp = 0;
-        for(int i=0; i < n; i++){  
-            for(int j=1; j < (n-i); j++){  
-                if(a[j-1] > a[j]){  
-                    //Intercambio  
-                    temp = a[j-1];  
-                    a[j-1] = a[j];  
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Iteracion del paso " + (i + 1) + ":");
+            for (int j = 1; j < (n - i); j++) {
+                if (a[j - 1] > a[j]) {
+                    // Intercambio
+                    temp = a[j - 1];
+                    a[j - 1] = a[j];
                     a[j] = temp;
-                    comp ++;
-                    }          
-                }  
-            } 
-        System.out.println("Array ordenado:");
-        this.MuestraArray(a);
-        System.out.println("Total comparaciones:" + comp);
+                    comp++;
+
+                    System.out.println("Comparando " + a[j - 1] + " y " + a[j] + ", intercambio realizado.");
+                    System.out.print("Arreglo actual: ");
+                    this.MuestraArray(a);
+                } else {
+                    System.out.println("Comparando " + a[j - 1] + " y " + a[j] + ", no se realiza intercambio.");
+                }
+            }
+            System.out.println("Final de la iteracion " + (i + 1) + ".");
         }
 
+        System.out.println("Array ordenado:");
+        this.MuestraArray(a);
+        System.out.println("Total comparaciones: " + comp);
+}
+
     public void SelectionSort(int[] a) {
+        System.out.println("Array Original: ");
         this.MuestraArray(a);
         int n = a.length;
         int comp = 0;
         for (int i = 0; i < n - 1; i++) {
             int indiceMinimo = i;
+            System.out.println("\nIteracion " + (i + 1) + ":");
+            System.out.println("Indice minimo inicial: " + indiceMinimo);
             for (int j = i + 1; j < n; j++) {
                 if (a[j] < a[indiceMinimo]) {
                     indiceMinimo = j;
                     comp ++;
-                    }
+                  }
                 }
-            // Intercambiar arreglo[i] 
-            //y arreglo[indiceMinimo]
-            int temp = a[i];
-            a[i] = a[indiceMinimo];
-            a[indiceMinimo] = temp;
+            // Intercambiar arreglo[i] y arreglo[indiceMinimo]
+            if (indiceMinimo != i) {
+                int temp = a[i];
+                a[i] = a[indiceMinimo];
+                a[indiceMinimo] = temp;
+                System.out.println("Intercambio: " + a[i] + " <-> " + a[indiceMinimo]);
+            }else {
+                System.out.println("No se realizo intercambio");
+            }
            }
         System.out.println("Array ordenado:");
         this.MuestraArray(a);
@@ -55,26 +73,36 @@ public class Ordenamientos {
         } 
         
     public void InsertionSort(int[] a) {
+        System.out.println("Array Original:");
         this.MuestraArray(a);
         int n = a.length;
         int comp = 0;
+
         for (int i = 1; i < n; i++) {
             int valorActual = a[i];
             int j = i - 1;
+
+            System.out.println("\nIteracion " + i + ":");
+
             while (j >= 0 && a[j] > valorActual) {
                 a[j + 1] = a[j];
                 j--;
-                comp ++;
-                }
-            a[j + 1] = valorActual;
+                comp++;
             }
-        System.out.println("Array ordenado:");
-        this.MuestraArray(a);
-        System.out.println("Total comparaciones:" + comp);
+
+            a[j + 1] = valorActual;
+
+            System.out.println("Estado actual del arreglo:");
+            this.MuestraArray(a);
         }
+        System.out.println("\nArray ordenado:");
+        this.MuestraArray(a);
+        System.out.println("Total comparaciones:" + comp);      
+    }   
 
     public void ShellSort(int[] a) {
-        this.MuestraArray(a);        
+        System.out.println("Array Original:");
+        this.MuestraArray(a);
         int n = a.length;
         int incremento = n;
         int comp = 0;
@@ -83,44 +111,59 @@ public class Ordenamientos {
             for (int k = 0; k < incremento; k++) {
                 for (int i = incremento + k; i < n; i += incremento) {
                     int j = i;
+
                     while (j - incremento >= 0 && a[j] < a[j - incremento]) {
                         int tmp = a[j];
                         a[j] = a[j - incremento];
                         a[j - incremento] = tmp;
                         j -= incremento;
-                        comp ++;
-                        }
+                        comp++;
+
+                        System.out.println("\nIncremento: " + incremento);
+                        System.out.println("Estado actual del arreglo:");
+                        this.MuestraArray(a);
                     }
                 }
-            } 
-        while (incremento > 1);
-        System.out.println("Array ordenado:");
+            }
+
+        } while (incremento > 1);
+
+        System.out.println("\nArray ordenado:");
         this.MuestraArray(a);
-        System.out.println("Total comparaciones:" + comp);
-        }
-    
+
+        System.out.println("Total comparaciones: " + comp);
+    }
+    private int comparisons_1 = 0;
+    public int get_comparisons_1(){
+        return this.comparisons_1;
+    }
     public void mergeSort(int[] a) {
-        if (a.length < 2) { 
+        System.out.println("Array Original:");
+        this.MuestraArray(a);
+
+        if (a.length < 2) {
             return;
         }
+
         int mid = a.length / 2;
         int[] left = new int[mid];
         int[] right = new int[a.length - mid];
- 
+
         System.arraycopy(a, 0, left, 0, mid);
         System.arraycopy(a, mid, right, 0, a.length - mid);
- 
+
         mergeSort(left);
         mergeSort(right);
         merge(a, left, right);
-        System.out.println("Array ordenado:");
+
+        System.out.println("\nArray ordenado:");
         this.MuestraArray(a);
-        System.out.println("Numero de comparaciones:"+ (int)(a.length*(Math.log(a.length)/Math.log(2))-(a.length - 1)));
-        }
-     
-    
+        System.out.println("Total comparaciones: " + this.get_comparisons_1());
+    }
+
     public void merge(int[] arr, int[] left, int[] right) {
         int i = 0, j = 0, k = 0;
+
         while (i < left.length && j < right.length) {
             if (left[i] <= right[j]) {
                 arr[k] = left[i];
@@ -130,19 +173,63 @@ public class Ordenamientos {
                 j++;
             }
             k++;
+            this.comparisons_1++;
+
+            // Mostrar el estado actual del arreglo después de cada comparación
+            System.out.println("\nComparacion:");
+            System.out.println("Estado actual del arreglo:");
+            this.MuestraArray(arr);
         }
+
         while (i < left.length) {
             arr[k] = left[i];
             i++;
             k++;
         }
+
         while (j < right.length) {
             arr[k] = right[j];
             j++;
             k++;
         }
     }
+    
+    private int comparisons_2 = 0;
+    public int get_comparisons_2(){
+        return this.comparisons_2;
+    }
+    public void quicksort(int[] array, int low, int high) {
+        int i = low;
+        int j = high;
+        int pivot = array[low + (high - low) / 2];
 
+        while (i <= j) {
+            while (array[i] < pivot) {
+                i++;
+                comparisons_2++; // Incrementa el contador de comparaciones
+            }
+            while (array[j] > pivot) {
+                j--;
+                comparisons_2++; // Incrementa el contador de comparaciones
+            }
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
 
-                
+                // Muestra el estado actual del arreglo después de cada intercambio
+                System.out.println("\nIntercambio:");
+                this.MuestraArray(array);
+            }
+        }
+
+        if (low < j) {
+            quicksort(array, low, j);
+        }
+        if (i < high) {
+            quicksort(array, i, high);
+        }
+    }
 }
